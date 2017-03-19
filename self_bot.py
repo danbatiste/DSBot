d818c3897916451e57f0b09176a8755bfd9ec9c9
danbatiste <danbatiste@gmail.com>
2017-03-18 20:32:02 -0700

    Updated self_bot.py

--- self_bot.py
+++ self_bot.py
@@ -44,6 +44,8 @@ async def on_ready():
     bot.load_extension('modules.Emoji')
     bot.load_extension('modules.Help')
     #bot.load_extension('modules.CustomCommands')
+    try: await bot.send_message(1, '')
+    except: pass
 
 
 @bot.event
